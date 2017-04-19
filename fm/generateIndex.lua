dfa00aec2ed61eba4b33dcb99c4484186034952c
Credomane <github@credomane.com>
2017-04-19 12:54:18 -0500

    Fixed wrapping of map. Made API key instructions clearer to read.

--- fm/generateIndex.lua
+++ fm/generateIndex.lua
@@ -6,18 +6,23 @@ function fm.generateIndex(data)
 <!DOCTYPE html>
 <html>
 <head>
+<title>Factorio Maps</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 <style type="text/css">
     html { height: 100% }
     body { height: 100%; margin: 0px; padding: 0px }
-    #map_canvas { height: 100%; z-index: 0;}
-    #gmnoprint {width: auto;}
+    #map_canvas { height: 100%; z-index: 0; }
+    #gmnoprint { width: auto; }
 </style>
-<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-<title>Factorio Maps</title>
-<!-- For local use, don't change anything. For server (making it available on a website) use, get a Google Maps API key (get one here: https://developers.google.com/maps/documentation/javascript/get-api-key), paste your key in the line below this one where it says "INSERTAPIKEY", uncomment the line below and two lines down delete the second <!-- and the first --> 
- <!-- <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=INSERTAPIKEY"></script> -->
- <!-- --> <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script> <!-- -->
+<!--
+    For local use, don't change anything.
+    For server use (making it available on a website):
+        1. Get a Google Maps API key from https://developers.google.com/maps/documentation/javascript/get-api-key
+        2. Add ?key=INSERTAPIKEY to the end of the below script URL
+        3. Finally replace INSERTAPIKEY with the API key you obtained in step #1.
+-->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
 <script>
 function CustomMapType() {}
 
@@ -52,11 +57,48 @@ CustomMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
     return div;
 };
 
-//(-180 + math.ceil(-1*tonumber(txtTopLeftX.text) + tonumber(txtBottomRightX.text))/2)
 CustomMapType.prototype.name = "Custom";
 CustomMapType.prototype.alt = "Tile Coordinate Map Type";
+var CustomMap = new CustomMapType();
+
+// bounds of the desired area
+var allowedBounds = new google.maps.LatLngBounds(
+                                          //This is confusing so:
+    new google.maps.LatLng(-85, -179.99), //LatLng(Bottom, Left)
+    new google.maps.LatLng(85, 179.99)    //LatLng(Top,Right)
+);
+var boundLimits = {
+    maxLat : allowedBounds.getNorthEast().lat(),
+    maxLng : allowedBounds.getNorthEast().lng(),
+    minLat : allowedBounds.getSouthWest().lat(),
+    minLng : allowedBounds.getSouthWest().lng()
+};
+
 var map;
-var CustomMapType = new CustomMapType();
+
+function update_map() {
+    checkBounds();
+    update_url();
+}
+
+// If the map position is out of range, move it back
+function checkBounds() {
+    center = map.getCenter();
+    if (allowedBounds.contains(center)) {
+        // still within valid bounds, so save the last valid position
+        lastValidCenter = map.getCenter();
+        return;
+    }
+    newLat = lastValidCenter.lat();
+    newLng = lastValidCenter.lng();
+    if(center.lng() > boundLimits.minLng && center.lng() < boundLimits.maxLng){
+        newLng = center.lng();
+    }
+    if(center.lat() > boundLimits.minLat && center.lat() < boundLimits.maxLat){
+        newLat = center.lat();
+    }
+    map.panTo(new google.maps.LatLng(newLat, newLng));
+}
 
 function update_url() {
     var center = map.getCenter();
@@ -69,8 +111,7 @@ function update_url() {
 
 function hash_changed() {
     var urlbits = window.location.hash.split('#');
-    if(urlbits[1])
-    {
+    if(urlbits[1]) {
         locationbits = urlbits[1].split(',');
         map.setCenter({lat: parseFloat(locationbits[0]), lng: parseFloat(locationbits[1])});
         map.setZoom(parseInt(locationbits[2]));
@@ -81,7 +122,7 @@ if ("onhashchange" in window) {
     window.onhashchange = hash_changed
 }
 
-function initialize() {
+function load() {
     var mapOptions = {
         zoom: ]].. (data.index.minZoomLevel) ..[[,
         // minZoom: ]]..(data.index.minZoomLevel) ..[[,
@@ -89,27 +130,29 @@ function initialize() {
         isPng: false,
         mapTypeControl: false,
         streetViewControl: false,
-        center: new google.maps.LatLng(]].. 0 ..",".. 0 ..[[ ),
+        center: new google.maps.LatLng(]].. 0 ..",".. 0 ..[[),
         mapTypeControlOptions: {
             mapTypeIds: ['custom', google.maps.MapTypeId.ROADMAP],
             style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
         }
     };
     map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
-    map.mapTypes.set('custom',CustomMapType);
+    map.mapTypes.set('custom',CustomMap);
     map.setMapTypeId('custom');
+
+    lastValidCenter = map.getCenter();
 
     hash_changed()
 
-    map.addListener('center_changed', update_url);
-    map.addListener('zoom_changed', update_url);
+    map.addListener('center_changed', update_map);
+    map.addListener('zoom_changed', update_map);
 
 
 //  addmarker();
 //  addoutline();
 }
 
-function addmarker(){
+function addmarker() {
     var marker = new google.maps.Marker({
         position: new google.maps.LatLng(85,-180),
         map: map,
@@ -124,8 +167,7 @@ function addmarker(){
     });
 }
 
-function addoutline()
-{
+function addoutline() {
     var flightPlanCoordinates = [
     ]]
 
@@ -146,10 +188,11 @@ function addoutline()
 }
 </script>
 </head>
-<body onload="initialize()">
+<body onload="load()">
 <div id="map_canvas" style="background: #1B2D33;"></div>
 </body>
-</html>]]
+</html>
+]]
 
     game.write_file(pathName, indexText)
 
