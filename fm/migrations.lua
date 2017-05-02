47b6e1a1634cf7ee64d34aa1a32274dc486fe742
Credomane <github@credomane.com>
2017-05-02 11:14:11 -0500

    Update migrations code to fix misdetecting newer versions as older.

--- fm/migrations.lua
+++ fm/migrations.lua
@@ -1,23 +1,34 @@
 
 fm.migrations = {}
 
-function fm.migrations.doUpdate(oldVersion, newVersion)
-    Game.print_all("[FactorioMaps] Updater: Version changed from " .. oldVersion .. " to " .. newVersion .. ".")
+function fm.migrations.doUpdate(oldVersionString, newVersionString)
+    Game.print_all("[FactorioMaps] Updater: Version changed from " .. oldVersionString .. " to " .. newVersionString .. ".")
+
+    local oldVersionTmp = oldVersionString.split(".")
+    local newVersionTmp = newVersionString.split(".")
+
+    --Gives a buffer of 1000 for each section of Factorio's "simver" (Major.Minor.Build)
+    local oldVersion = oldVersionTmp[1] * 1000000 + oldVersionTmp[2] * 1000 + oldVersionTmp[3]
+    local newVersion = newVersionTmp[1] * 1000000 + newVersionTmp[2] * 1000 + newVersionTmp[3]
 
     if oldVersion > newVersion then
         Game.print_all("[FactorioMaps] Updater: Version downgrade detected. I can't believe you've done this.")
+        Game.print_all("[FactorioMaps] Updater: I don't have handlers for downgrades so things might break. :(")
         return
     end
 
-    if oldVersion < "0.7.0" then
+    if oldVersion < 7000 then
+        Game.print_all("[FactorioMaps] Updater: Migrating to 0.7.0...")
         fm.migrations.to_0_7_0()
     end
 
-    if oldVersion < "0.7.1" then
+    if oldVersion < 7001 then
+        Game.print_all("[FactorioMaps] Updater: Migrating to 0.7.1...")
         fm.migrations.to_0_7_1()
     end
 
-    if oldVersion < "0.15.1" then
+    if oldVersion < 15001 then
+        Game.print_all("[FactorioMaps] Updater: Migrating to 0.15.1...")
         fm.migrations.to_0_15_1()
     end
 
