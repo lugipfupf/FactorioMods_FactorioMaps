62529fbb3c67b830b6abcb8e57c2c3132c794f78
Jacob Hands <jacob@gogit.io>
2018-06-11 19:59:37 -0500

    Bigger Array

--- fm/generateMap.lua
+++ fm/generateMap.lua
@@ -22,7 +22,7 @@ function fm.generateMap(data)
     local minZoomLevel = data.gridSizeIndex
     local maxZoomLevel = 0 -- default
 
-    local resolutionArray = {8,16,32,64,128,256,512,1024,2048,4096,8192} -- resolution for each zoom level, lvl 0 is always 8x8 (256x256 pixels)
+    local resolutionArray = {8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576} -- resolution for each zoom level, lvl 0 is always 8x8 (256x256 pixels)
 
     local tmpCounter = 0 -- in google maps, max zoom out level is 0, so start with 0
     for _, resolution in pairs(resolutionArray) do
